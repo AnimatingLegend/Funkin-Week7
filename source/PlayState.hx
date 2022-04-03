ae9feb1ab20bde7a93ce85c617009bb0d5378133
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-04-03 10:09:27 -0400

    Fixed Camera follow speed, Archived things

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -499,135 +499,135 @@ class PlayState extends MusicBeatState
 		                  bg.scale.set(6, 6);
 		                  add(bg);
 
-					}
-					case 'ugh' | 'guns':
-					{
-						defaultCamZoom = 0.9;
+				}
+				case 'ugh' | 'guns':
+				{
+					defaultCamZoom = 0.9;
 
-						curStage = 'warzone';
-						var sky:FlxSprite = new FlxSprite(-400,-400).loadGraphic(Paths.image('warzone/tankSky'));
-						sky.scrollFactor.set(0, 0);
-						sky.antialiasing = true;
-						sky.setGraphicSize(Std.int(sky.width * 1.5));
-						add(sky);
+					curStage = 'warzone';
+					var sky:FlxSprite = new FlxSprite(-400,-400).loadGraphic(Paths.image('warzone/tankSky'));
+					sky.scrollFactor.set(0, 0);
+					sky.antialiasing = true;
+					sky.setGraphicSize(Std.int(sky.width * 1.5));
+					add(sky);
 					
-						var clouds:FlxSprite = new FlxSprite(FlxG.random.int(-700, -100), FlxG.random.int(-20, 20)).loadGraphic(Paths.image('warzone/tankClouds'));
-						clouds.scrollFactor.set(0.1, 0.1);
-						clouds.velocity.x = FlxG.random.float(5, 15);
-						clouds.antialiasing = true;
-						clouds.updateHitbox();
-						add(clouds);
+					var clouds:FlxSprite = new FlxSprite(FlxG.random.int(-700, -100), FlxG.random.int(-20, 20)).loadGraphic(Paths.image('warzone/tankClouds'));
+					clouds.scrollFactor.set(0.1, 0.1);
+					clouds.velocity.x = FlxG.random.float(5, 15);
+					clouds.antialiasing = true;
+					clouds.updateHitbox();
+					add(clouds);
 					
-						var mountains:FlxSprite = new FlxSprite(-300,-20).loadGraphic(Paths.image('warzone/tankMountains'));
-						mountains.scrollFactor.set(0.2, 0.2);
-						mountains.setGraphicSize(Std.int(1.2 * mountains.width));
-						mountains.updateHitbox();
-						mountains.antialiasing = true;
-						add(mountains);
+					var mountains:FlxSprite = new FlxSprite(-300,-20).loadGraphic(Paths.image('warzone/tankMountains'));
+					mountains.scrollFactor.set(0.2, 0.2);
+					mountains.setGraphicSize(Std.int(1.2 * mountains.width));
+					mountains.updateHitbox();
+					mountains.antialiasing = true;
+					add(mountains);
 					
-						var buildings:FlxSprite = new FlxSprite(-200,0).loadGraphic(Paths.image('warzone/tankBuildings'));
-						buildings.scrollFactor.set(0.3, 0.3);
-						buildings.setGraphicSize(Std.int(buildings.width * 1.1));
-						buildings.updateHitbox();
-						buildings.antialiasing = true;
-						add(buildings);
+					var buildings:FlxSprite = new FlxSprite(-200,0).loadGraphic(Paths.image('warzone/tankBuildings'));
+					buildings.scrollFactor.set(0.3, 0.3);
+					buildings.setGraphicSize(Std.int(buildings.width * 1.1));
+					buildings.updateHitbox();
+					buildings.antialiasing = true;
+					add(buildings);
 					
-						var ruins:FlxSprite = new FlxSprite(-200,0).loadGraphic(Paths.image('warzone/tankRuins'));
-						ruins.scrollFactor.set(0.35, 0.35);
-						ruins.setGraphicSize(Std.int(ruins.width * 1.1));
-						ruins.updateHitbox();
-						ruins.antialiasing = true;
-						add(ruins);
+					var ruins:FlxSprite = new FlxSprite(-200,0).loadGraphic(Paths.image('warzone/tankRuins'));
+					ruins.scrollFactor.set(0.35, 0.35);
+					ruins.setGraphicSize(Std.int(ruins.width * 1.1));
+					ruins.updateHitbox();
+					ruins.antialiasing = true;
+					add(ruins);
 	
-						var smokeLeft:FlxSprite = new FlxSprite(-200,-100);
-						smokeLeft.frames = Paths.getSparrowAtlas('warzone/smokeLeft');
-						smokeLeft.animation.addByPrefix('idle', 'SmokeBlurLeft ', 24, true);
-						smokeLeft.scrollFactor.set(0.4, 0.4);
-						smokeLeft.antialiasing = true;
-						smokeLeft.animation.play('idle');
-						add(smokeLeft);
+					var smokeLeft:FlxSprite = new FlxSprite(-200,-100);
+					smokeLeft.frames = Paths.getSparrowAtlas('warzone/smokeLeft');
+					smokeLeft.animation.addByPrefix('idle', 'SmokeBlurLeft ', 24, true);
+					smokeLeft.scrollFactor.set(0.4, 0.4);
+					smokeLeft.antialiasing = true;
+					smokeLeft.animation.play('idle');
+					add(smokeLeft);
 					
-						var smokeRight:FlxSprite = new FlxSprite(1100,-100);
-						smokeRight.frames = Paths.getSparrowAtlas('warzone/smokeRight');
-						smokeRight.animation.addByPrefix('idle', 'SmokeRight ', 24, true);
-						smokeRight.scrollFactor.set(0.4, 0.4);
-						smokeRight.antialiasing = true;
-						smokeRight.animation.play('idle');							
-						add(smokeRight);
+					var smokeRight:FlxSprite = new FlxSprite(1100,-100);
+					smokeRight.frames = Paths.getSparrowAtlas('warzone/smokeRight');
+					smokeRight.animation.addByPrefix('idle', 'SmokeRight ', 24, true);
+					smokeRight.scrollFactor.set(0.4, 0.4);
+					smokeRight.antialiasing = true;
+					smokeRight.animation.play('idle');							
+					add(smokeRight);
 
-						var tankRolling = new FlxSprite(300, 500);
-						tankRolling.frames = Paths.getSparrowAtlas('tank/tankRolling');
-						tankRolling.animation.addByPrefix('idle', 'BG tank w lighting instance ', 24, true);
-						tankRolling.scrollFactor.set(1,1);
-						tankRolling.antialiasing = true;
-						tankRolling.animation.play('idle');
-						add(tankRolling);
+					var tankRolling = new FlxSprite(300, 500);
+					tankRolling.frames = Paths.getSparrowAtlas('tank/tankRolling');
+					tankRolling.animation.addByPrefix('idle', 'BG tank w lighting instance ', 24, true);
+					tankRolling.scrollFactor.set(1,1);
+					tankRolling.antialiasing = true;
+					tankRolling.animation.play('idle');
+					add(tankRolling);
 	
-					    tower = new FlxSprite(100, 120);
-						tower.frames = Paths.getSparrowAtlas('warzone/tankWatchtower');
-						tower.animation.addByPrefix('idle', 'watchtower gradient color', 24, false);
-						tower.antialiasing = true;
-						add(tower);
+				    tower = new FlxSprite(100, 120);
+					tower.frames = Paths.getSparrowAtlas('warzone/tankWatchtower');
+					tower.animation.addByPrefix('idle', 'watchtower gradient color', 24, false);
+					tower.antialiasing = true;
+					add(tower);
 					
-						var ground:FlxSprite = new FlxSprite(-420, -150).loadGraphic(Paths.image('warzone/tankGround'));
-						ground.scrollFactor.set();
-						ground.antialiasing = true;
-						ground.setGraphicSize(Std.int(ground.width * 1.15));
-						ground.scrollFactor.set(1, 1);
-						ground.updateHitbox();
-						add(ground);
+					var ground:FlxSprite = new FlxSprite(-420, -150).loadGraphic(Paths.image('warzone/tankGround'));
+					ground.scrollFactor.set();
+					ground.antialiasing = true;
+					ground.setGraphicSize(Std.int(ground.width * 1.15));
+					ground.scrollFactor.set(1, 1);
+					ground.updateHitbox();
+					add(ground);
 
-						// TankBop Shit
+					// TankBop Shit
 					
-						tankBop1 = new FlxSprite(-500,650);
-						tankBop1.frames = Paths.getSparrowAtlas('tank/tank0');
-						tankBop1.animation.addByPrefix('boppin 1', 'fg tankhead far right', 24, false);
-						tankBop1.scrollFactor.set(1.7, 1.5);
-						tankBop1.antialiasing = true;
-						tankBop1.updateHitbox();
-						add(tankBop1);
+					tankBop1 = new FlxSprite(-500,650);
+					tankBop1.frames = Paths.getSparrowAtlas('tank/tank0');
+					tankBop1.animation.addByPrefix('boppin 1', 'fg tankhead far right', 24, false);
+					tankBop1.scrollFactor.set(1.7, 1.5);
+					tankBop1.antialiasing = true;
+					tankBop1.updateHitbox();
+					add(tankBop1);
 									
 					
-						tankBop2 = new FlxSprite(-300,750);
-						tankBop2.frames = Paths.getSparrowAtlas('tank/tank1');
-						tankBop2.animation.addByPrefix('boppin 2','fg tankhead 5', 24, false);
-						tankBop2.scrollFactor.set(2.0, 0.2);
-						tankBop2.antialiasing = true;
-						tankBop2.updateHitbox();
-						add(tankBop2);		
+					tankBop2 = new FlxSprite(-300,750);
+					tankBop2.frames = Paths.getSparrowAtlas('tank/tank1');
+					tankBop2.animation.addByPrefix('boppin 2','fg tankhead 5', 24, false);
+					tankBop2.scrollFactor.set(2.0, 0.2);
+					tankBop2.antialiasing = true;
+					tankBop2.updateHitbox();
+					add(tankBop2);		
 					
-						tankBop3 = new FlxSprite(450,940);
-						tankBop3.frames = Paths.getSparrowAtlas('tank/tank2');
-						tankBop3.animation.addByPrefix('boppin 3','foreground man 3', 24, false);
-						tankBop3.scrollFactor.set(1.5, 1.5);
-						tankBop3.antialiasing = true;
-						tankBop3.updateHitbox();
-						add(tankBop3);
+					tankBop3 = new FlxSprite(450,940);
+					tankBop3.frames = Paths.getSparrowAtlas('tank/tank2');
+					tankBop3.animation.addByPrefix('boppin 3','foreground man 3', 24, false);
+					tankBop3.scrollFactor.set(1.5, 1.5);
+					tankBop3.antialiasing = true;
+					tankBop3.updateHitbox();
+					add(tankBop3);
 									
-						tankBop4 = new FlxSprite(1300,1200);
-						tankBop4.frames = Paths.getSparrowAtlas('tank/tank3');
-						tankBop4.animation.addByPrefix('boppin 4','fg tankhead 4', 24, false);
-						tankBop4.scrollFactor.set(3.5, 2.5);
-						tankBop4.antialiasing = true;
-						tankBop4.updateHitbox();
-						add(tankBop4);
+					tankBop4 = new FlxSprite(1300,1200);
+					tankBop4.frames = Paths.getSparrowAtlas('tank/tank3');
+					tankBop4.animation.addByPrefix('boppin 4','fg tankhead 4', 24, false);
+					tankBop4.scrollFactor.set(3.5, 2.5);
+					tankBop4.antialiasing = true;
+					tankBop4.updateHitbox();
+					add(tankBop4);
 								
 					
-						tankBop5 = new FlxSprite(1300,900);
-						tankBop5.frames = Paths.getSparrowAtlas('tank/tank4');
-						tankBop5.animation.addByPrefix('boppin 5','fg tankman bobbin 3', 24, false);
-						tankBop5.scrollFactor.set(1.5, 1.5);
-						tankBop5.antialiasing = true;
-						tankBop5.updateHitbox();
-						add(tankBop5);
+					tankBop5 = new FlxSprite(1300,900);
+					tankBop5.frames = Paths.getSparrowAtlas('tank/tank4');
+					tankBop5.animation.addByPrefix('boppin 5','fg tankman bobbin 3', 24, false);
+					tankBop5.scrollFactor.set(1.5, 1.5);
+					tankBop5.antialiasing = true;
+					tankBop5.updateHitbox();
+					add(tankBop5);
 					
-						tankBop6 = new FlxSprite(1620,700);
-						tankBop6.frames = Paths.getSparrowAtlas('tank/tank5');
-						tankBop6.animation.addByPrefix('boppin 6','fg tankhead far right', 24, false);
-						tankBop6.scrollFactor.set(1.5, 1.5);
-						tankBop6.antialiasing = true;
-						tankBop6.updateHitbox();
-						add(tankBop6);
+					tankBop6 = new FlxSprite(1620,700);
+					tankBop6.frames = Paths.getSparrowAtlas('tank/tank5');
+					tankBop6.animation.addByPrefix('boppin 6','fg tankhead far right', 24, false);
+					tankBop6.scrollFactor.set(1.5, 1.5);
+					tankBop6.antialiasing = true;
+					tankBop6.updateHitbox();
+					add(tankBop6);
 
 				}
 				case 'stress':
@@ -865,7 +865,7 @@ class PlayState extends MusicBeatState
 				dad.y += 100;
 				camPos.set(dad.getGraphicMidpoint().x + 300, dad.getGraphicMidpoint().y);
 			case 'tankman':
-				dad.y += 215;	
+				dad.y += 215;
 			case 'legend':
 				dad.y += 53;
 		}
@@ -896,17 +896,14 @@ class PlayState extends MusicBeatState
 			case 'schoolEvil':
 				var evilTrail = new FlxTrail(dad, null, 4, 24, 0.3, 0.069);
 				add(evilTrail);
-
 				boyfriend.x += 200;
 				boyfriend.y += 220;
 				gf.x += 180;
 				gf.y += 300;
-
 				case 'warzone':
 				gf.y += -55;
 				gf.x -= 200;
 				boyfriend.x += 40;
-
 				case 'warzone-stress':
 				gf.y += -155;
 				gf.x -= 90;
@@ -950,8 +947,8 @@ class PlayState extends MusicBeatState
 
 		add(camFollow);
 
-		FlxG.camera.follow(camFollow, LOCKON, 0.04);
 		FlxG.camera.zoom = defaultCamZoom;
+		FlxG.camera.follow(camFollow, LOCKON, 0.02);
 		FlxG.camera.focusOn(camFollow.getPosition());
 
 		FlxG.worldBounds.set(0, 0, FlxG.width, FlxG.height);
@@ -2132,7 +2129,17 @@ class PlayState extends MusicBeatState
 
 			if (combo >= 10 || combo == 0)
 				add(numScore);
-				
+
+		/*	if (combo >= 10)
+				{
+					add(comboSpr);
+				}
+
+			if (combo == 0)
+				{
+					comboSpr.destroy();
+				}		
+		*/		
 			FlxTween.tween(numScore, {alpha: 0}, 0.2, {
 				onComplete: function(tween:FlxTween)
 				{
@@ -2669,33 +2676,6 @@ class PlayState extends MusicBeatState
 				tankBop6.animation.play('boppin 6', true);
 				tower.animation.play('idle', true);
 
-				// Left Tankmen spawn
-						/*if (curBeat == tankStep.left)
-							{
-								var tankmanRunner:BackgroundTankmen = new BackgroundTankmen();
-								tankmanRunner.resetShit(FlxG.random.int(630, 730) * -1, 255, true, 1, 1.5);
-							}
-				
-				// Right Tankmen spawn
-						if (curBeat == tankStep.right)
-						{
-							var tankmanRunner:BackgroundTankmen = new BackgroundTankmen();
-							tankmanRunner.resetShit(FlxG.random.int(1500, 1700) * 1, 275, true, 1, 1.5);
-						}
-
-				// Pico Right
-						if (curBeat == picoStep.right)
-						{
-							gf.playAnim('shoot' + FlxG.random.int(1, 2), false);
-							var tankmanRunner:BackgroundTankmen = new BackgroundTankmen();
-						}
-	
-				// Pico Left
-						if (curBeat == picoStep.left)
-						{
-							gf.playAnim('shoot' + FlxG.random.int(3, 4), false);
-							var tankmanRunner:BackgroundTankmen = new BackgroundTankmen();
-						}*/
 
 			case 'limo':
 				grpLimoDancers.forEach(function(dancer:BackgroundDancer)
