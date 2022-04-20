bc0ad7ad13156a9f947a181a735a15d262ca2a3a
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-04-20 00:18:32 -0400

    i forgor :skull:

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -130,7 +130,7 @@ class TitleState extends MusicBeatState
 			// music.loadStream(Paths.music('freakyMenu'));
 			// FlxG.sound.list.add(music);
 			// music.play();
-			FlxG.sound.playMusic(Paths.music('freakyMenu'), 1);
+			FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 
 			FlxG.sound.music.fadeIn(4, 0, 0.7);
 		}
