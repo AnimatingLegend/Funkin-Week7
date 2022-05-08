b383f77d752b51a83a30e58a119e5ebf6a16e158
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-05-08 13:08:38 -0400

    Vaulted Legend

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -36,7 +36,6 @@ class HealthIcon extends FlxSprite
 		animation.add('parents-christmas', [17], 0, false, isPlayer);
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
-		animation.add('legend', [10,11], 0, false, isPlayer);
 		animation.play(char);
 		scrollFactor.set();
 	}
