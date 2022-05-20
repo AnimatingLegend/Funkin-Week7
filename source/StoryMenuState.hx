9feedd67553b2ef7cc5ad720075a11a4284479eb
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-05-19 23:07:09 -0400

    StoryMenu  Change | Pico flipX shit

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -166,7 +166,7 @@ class StoryMenuState extends MusicBeatState
 					weekCharacterThing.setGraphicSize(Std.int(weekCharacterThing.width * 0.5));
 					weekCharacterThing.updateHitbox();
 				case 'pico':
-					weekCharacterThing.flipX = false;
+					weekCharacterThing.flipX = true;
 				case 'parents-christmas':
 					weekCharacterThing.setGraphicSize(Std.int(weekCharacterThing.width * 0.9));
 					weekCharacterThing.updateHitbox();
