7eaf7b764798940d3324986af86ce16ea80f0d9d
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-07-17 19:34:16 -0400

    Update NoteSplash.hx

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -1,87 +1,45 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
-import Paths;
-import Song;
-import Conductor;
-import Math;
-import openfl.geom.Matrix;
-import openfl.display.BitmapData;
-import openfl.utils.AssetType;
-import lime.graphics.Image;
-import flixel.graphics.FlxGraphic;
-import openfl.utils.AssetManifest;
-import openfl.utils.AssetLibrary;
-import flixel.system.FlxAssets;
-import flixel.FlxBasic;
-import flixel.FlxG;
-import flixel.FlxGame;
-import flixel.FlxObject;
-import flixel.math.FlxMath;
-import flixel.math.FlxPoint;
-import flixel.math.FlxRect;
-import lime.utils.Assets;
-import openfl.geom.Matrix;
-import openfl.display.BitmapData;
-import openfl.utils.AssetType;
-import lime.graphics.Image;
-import flixel.graphics.FlxGraphic;
-import flixel.animation.FlxAnimation;
 
-import openfl.utils.AssetManifest;
-import openfl.utils.AssetLibrary;
-
-#if cpp
-import Sys;
-import sys.FileSystem;
-#end
-
-
-using StringTools;
-
-class NoteSplash extends FlxSprite // code taken from triangle engine :)
+class NoteSplash extends FlxSprite // Updated Code a lil bit, Code originally take from tr1angle engine.
 {
-    public function new(X:Float, Y:Float, noteData:Int)
-    {
-    	super(X, Y);
-    	frames = (Paths.getSparrowAtlas("noteSplashes"));
+	public function new(x:Float, y:Float, ?notedata:Int = 0)
+	{
+		super(x, y);
+		frames = Paths.getSparrowAtlas('noteSplashes');
 
-    	// impact 1
-    	animation.addByPrefix("note1-0", "note impact 1  blue", 24, false);
-    	animation.addByPrefix("note2-0", "note impact 1 green", 24, false);
-    	animation.addByPrefix("note0-0", "note impact 1 purple", 24, false);
-    	animation.addByPrefix("note3-0", "note impact 1 red", 24, false);
+		// impact 1
+		animation.addByPrefix('note1-0', 'note impact 1  blue', 24, false);
+		animation.addByPrefix('note2-0', 'note impact 1 green', 24, false);
+		animation.addByPrefix('note0-0', 'note impact 1 purple', 24, false);
+		animation.addByPrefix('note3-0', 'note impact 1 red', 24, false);
 
-    	// impact 2
-    	animation.addByPrefix("note1-1", "note impact 2 blue", 24, false);
-    	animation.addByPrefix("note2-1", "note impact 2 green", 24, false);
-    	animation.addByPrefix("note0-1", "note impact 2 purple", 24, false);
-    	animation.addByPrefix("note3-1", "note impact 2 red", 24, false);
+		// impact 2
+		animation.addByPrefix('note1-1', 'note impact 2 blue', 24, false);
+		animation.addByPrefix('note2-1', 'note impact 2 green', 24, false);
+		animation.addByPrefix('note0-1', 'note impact 2 purple', 24, false);
+		animation.addByPrefix('note3-1', 'note impact 2 red', 24, false);
+		setupNoteSplash(x, y, notedata);
+	}
 
-        setupNoteSplash(X, Y, noteData);
-    }
-
-    public function setupNoteSplash(x:Float, y:Float, note:Int)
-    {
-		this.x = x;
-		this.y = y;
-    	alpha = 0.6;
-    	animation.play("note" + note + "-" + FlxG.random.int(0, 1), true);
-
-		setPosition(x - Note.swagWidth * 0.95, y - Note.swagWidth);
-		offset.set(10, 10);
-
+	public function setupNoteSplash(x:Float, y:Float, ?notedata:Int = 0)
+	{
+		setPosition(x, y);
+		alpha = 0.6;
+		animation.play('note' + notedata + '-' + FlxG.random.int(0, 1), true);
+		animation.curAnim.frameRate += FlxG.random.int(-2, 2);
 		updateHitbox();
-    }
+		offset.set(width * 0.3, height * 0.3);
+	}
 
-    override public function update(elapsed:Float)
-    {
+	override public function update(elapsed:Float)
+	{
+		if (animation.curAnim.finished)
+			kill();
+			
 		super.update(elapsed);
-
-    	if(animation.curAnim.finished)
-    	{
-    		kill();
-    	}
-    }
+	}
 }
