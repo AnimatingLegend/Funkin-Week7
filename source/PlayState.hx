5924625cf54219d95e7d06831c85ff80bd1a553a
legendlol <83415030+LegendLOL@users.noreply.github.com>
2022-07-16 23:25:02 -0400

    MORE note input fixes

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2030,7 +2030,7 @@ class PlayState extends MusicBeatState
 		{
 			rating.y -= 25;
 			rating.screenCenter();
-			rating.scrollFactor.set(1,0);
+			rating.scrollFactor.set(0.7);
 
 			var scaleX = rating.scale.x;
 			var scaleY = rating.scale.y;
@@ -2050,7 +2050,7 @@ class PlayState extends MusicBeatState
 		{
 			comboSpr.y -= 25;
 			comboSpr.screenCenter();
-			comboSpr.scrollFactor.set(1,0);
+			comboSpr.scrollFactor.set(0.7);
 
 			var scaleX = rating.scale.x;
 			var scaleY = rating.scale.y;
@@ -2095,7 +2095,7 @@ class PlayState extends MusicBeatState
 				numScore.y += 50;
 				numScore.x -= 50;
 				numScore.screenCenter();
-				numScore.scrollFactor.set(1,0);
+				numScore.scrollFactor.set(0.7);
 
 				var scaleX = numScore.scale.x;
 				var scaleY = numScore.scale.y;
@@ -2189,12 +2189,9 @@ class PlayState extends MusicBeatState
 					{
 						for (possibleNote in possibleNotes)
 						{
-							if (possibleNote.noteData == daNote.noteData && Math.abs(daNote.strumTime - possibleNote.strumTime) < 10)
+							if (possibleNote.noteData == daNote.noteData && daNote.strumTime < possibleNote.strumTime)
 							{
-								removeList.push(daNote);
-							}
-							else if (possibleNote.noteData == daNote.noteData && daNote.strumTime < possibleNote.strumTime)
-							{
+								possibleNotes.remove(possibleNote);
 								possibleNotes.push(daNote);
 							}
 						}
@@ -2302,7 +2299,7 @@ class PlayState extends MusicBeatState
 			{
 				comboBr.y -= 25;
 				comboBr.screenCenter();
-				comboBr.scrollFactor.set(1,0);		
+				comboBr.scrollFactor.set(0.7);		
 			}
 			comboBr.x += 100;
 			comboBr.acceleration.y = 600;
